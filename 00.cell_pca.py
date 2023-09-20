56440767facc92faf9c762f075f44e9d316f1414
phildong <35715936+phildong@users.noreply.github.com>
2023-09-20 20:56:51 +0000

    add script for cell pca

---
+++ 00.cell_pca.py
@@ -0,0 +1,73 @@
+# %% imports and definition
+import os
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import xarray as xr
+from sklearn.decomposition import PCA
+
+from routine.utilities import classify_behav, load_mat_data
+
+IN_DPATH = "./data"
+IN_CELLMAP = "./data/CellMaps.xlsx"
+PARAM_BEHAV_WND = (-20 * 2.5, 20 * 2.5)
+SS_DICT = {}
+PARAM_NCOMP = 3
+PARAM_WHITEN = True
+PARAM_TRIM_UNKNOWN = False
+FIG_PATH = "./figs/cell_pca"
+
+# %% pca analysis and plot projections
+fig_path = os.path.join(FIG_PATH, "proj")
+os.makedirs(fig_path, exist_ok=True)
+for (anm, ss), act, behav_df in load_mat_data(IN_DPATH):
+    behav = behav_df.apply(classify_behav, axis="columns")
+    behav["evt"] = behav["event_map"] + "-" + behav["target"]
+    evt_tuning = []
+    for evt, evt_df in behav.groupby("evt"):
+        evt_arrs = []
+        for fm in evt_df["frame"]:
+            a = act.sel(
+                frame=slice(fm + PARAM_BEHAV_WND[0], fm + PARAM_BEHAV_WND[1])
+            ).mean("frame")
+            evt_arrs.append(a)
+        evt_arr = xr.concat(evt_arrs, dim="evt").mean("evt").assign_coords(event=evt)
+        evt_tuning.append(evt_arr)
+    evt_tuning = xr.concat(evt_tuning, "event")
+    if evt_tuning.sizes["event"] > PARAM_NCOMP:
+        pca = PCA(n_components=PARAM_NCOMP, whiten=PARAM_WHITEN)
+        proj = xr.DataArray(
+            pca.fit_transform(evt_tuning.transpose("unit_id", "event")),
+            dims=["unit_id", "comp"],
+            coords={
+                "unit_id": evt_tuning.coords["unit_id"],
+                "comp": ["comp{}".format(i) for i in range(PARAM_NCOMP)],
+            },
+        )
+        proj_df = proj.to_pandas()
+    else:
+        proj_df = evt_tuning.transpose("unit_id", "event").to_pandas()
+        proj_df.columns = ["comp{}".format(i) for i in range(len(proj_df.columns))]
+        proj_df["comp0"] = proj_df.get("comp0", 0)
+        proj_df["comp1"] = proj_df.get("comp1", 0)
+        proj_df["comp2"] = proj_df.get("comp2", 0)
+    proj_df = proj_df.reset_index()
+    proj_df["region"] = proj_df["unit_id"].map(
+        evt_tuning.coords["region"].to_series().to_dict()
+    )
+    if PARAM_TRIM_UNKNOWN:
+        proj_df = proj_df.dropna()
+    else:
+        proj_df["region"] = proj_df["region"].fillna("unknown")
+    fig = px.scatter_3d(
+        proj_df,
+        x="comp0",
+        y="comp1",
+        z="comp2",
+        color="region",
+        color_discrete_map={"unknown": "grey"},
+    )
+    fig.update_traces(marker_size=5)
+    fig.update_layout(legend={"itemsizing": "constant"})
+    fig.write_html(os.path.join(fig_path, "{}-{}.html".format(anm, ss)))
--- routine/utilities.py
+++ routine/utilities.py
@@ -22,54 +22,54 @@ DAY_DICT = {
 }
 COL_DICT = {"Day4_AccDay4_Test": "Day4"}
 
-PARAM_BEHAV_ORD = [
-    "Familiar-BeingAttacked",
-    "Novel-BeingAttacked",
-    "BeingAttacked",
-    "Familiar-SideAttack",
-    "Novel-SideAttack",
-    "SideAttack",
-    "Familiar-AggressiveChase",
-    "Novel-AggressiveChase",
-    "AggressiveChase",
-    "Familiar-Groupsniff_20fps",
-    "Novel-Groupsniff_20fps",
-    "Groupsniff_20fps",
-    "Familiar-SideSniff1_20fps",
-    "Novel-SideSniff1_20fps",
-    "SideSniff1_20fps",
-    "Familiar-dig_20fps",
-    "Novel-dig_20fps",
-    "dig_20fps",
-    "Familiar-nose2nose1_20fpsr",
-    "Novel-nose2nose1_20fpsr",
-    "nose2nose1_20fpsr",
-    "Familiar-nose2rear1_20fps",
-    "Novel-nose2rear1_20fps",
-    "nose2rear1_20fps",
-    "Familiar-nosey1_20fps",
-    "Novel-nosey1_20fps",
-    "nosey1_20fps",
-    "Familiar-follow1_20fps",
-    "Novel-follow1_20fps",
-    "follow1_20fps",
-    "Familiar-groomfront_20fps",
-    "Novel-groomfront_20fps",
-    "groom_front",
-    "groomfront_20fps",
-    "sitcorner",
-    "sitcorner_20fps",
-    "wallclimb",
-    "wallclimb_20fps",
-    "walk",
-    "walk_20fps",
-    "still",
-    "still_20fps",
-]
+PARAM_BEHAV_MAP = {
+    "Familiar-BeingAttacked": "attack",
+    "Novel-BeingAttacked": "attack",
+    "BeingAttacked": "attack",
+    "Familiar-SideAttack": "attack",
+    "Novel-SideAttack": "attack",
+    "SideAttack": "attack",
+    "Familiar-AggressiveChase": "aggression",
+    "Novel-AggressiveChase": "aggression",
+    "AggressiveChase": "aggression",
+    "Familiar-Groupsniff_20fps": "sniff",
+    "Novel-Groupsniff_20fps": "sniff",
+    "Groupsniff_20fps": "sniff",
+    "Familiar-SideSniff1_20fps": "sniff",
+    "Novel-SideSniff1_20fps": "sniff",
+    "SideSniff1_20fps": "sniff",
+    "Familiar-dig_20fps": "social",
+    "Novel-dig_20fps": "social",
+    "dig_20fps": "social",
+    "Familiar-nose2nose1_20fpsr": "sniff",
+    "Novel-nose2nose1_20fpsr": "sniff",
+    "nose2nose1_20fpsr": "sniff",
+    "Familiar-nose2rear1_20fps": "sniff",
+    "Novel-nose2rear1_20fps": "sniff",
+    "nose2rear1_20fps": "sniff",
+    "Familiar-nosey1_20fps": "sniff",
+    "Novel-nosey1_20fps": "sniff",
+    "nosey1_20fps": "sniff",
+    "Familiar-follow1_20fps": "social",
+    "Novel-follow1_20fps": "social",
+    "follow1_20fps": "social",
+    "Familiar-groomfront_20fps": "groom",
+    "Novel-groomfront_20fps": "groom",
+    "groom_front": "groom",
+    "groomfront_20fps": "groom",
+    "sitcorner": "still",
+    "sitcorner_20fps": "still",
+    "wallclimb": "move",
+    "wallclimb_20fps": "move",
+    "walk": "move",
+    "walk_20fps": "move",
+    "still": "still",
+    "still_20fps": "still",
+}
 
 
 def behav_key(k):
-    return list(map(PARAM_BEHAV_ORD.index, k))
+    return list(map(list(PARAM_BEHAV_MAP.keys()).index, k))
 
 
 def classify_behav(row):
@@ -77,6 +77,7 @@ def classify_behav(row):
     row = row.drop("frame").sort_index(key=behav_key)
     if row.max() == 1:
         evt = row.idxmax()
+        evt_mp = PARAM_BEHAV_MAP[evt]
         if evt.endswith("_20fps"):
             evt = evt[:-6]
         if evt.endswith("_20fpsr"):
@@ -91,8 +92,9 @@ def classify_behav(row):
             tgt = "self"
     else:
         evt = np.nan
+        evt_mp = np.nan
         tgt = np.nan
-    return pd.Series({"frame": fm, "event": evt, "target": tgt})
+    return pd.Series({"frame": fm, "event": evt, "target": tgt, "event_map": evt_mp})
 
 
 def load_mat_data(dpath):
