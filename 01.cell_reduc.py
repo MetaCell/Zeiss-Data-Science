d5a523e977afd762bd527982b9e9e3edd2a884f0
phildong <35715936+phildong@users.noreply.github.com>
2023-10-06 15:39:58 +0000

    update cell reduction analysis to include both trim and non-trim plots

--- 01.cell_reduc.py
+++ 01.cell_reduc.py
@@ -16,8 +16,7 @@ PARAM_BEHAV_WND = (-20 * 2.5, 20 * 2.5)
 SS_DICT = {}
 PARAM_NCOMP = 3
 PARAM_NNB = 5
-PARAM_WHITEN = True
-PARAM_TRIM_UNKNOWN = True
+PARAM_WHITEN = False
 PARAM_SIGMA = 4
 FIG_PATH = "./figs/cell_reduc"
 OUT_PATH = "./intermediate/cell_reduc"
@@ -60,71 +59,83 @@ tuning_df.to_feather(os.path.join(OUT_PATH, "tuning_df.feat"))
 
 # %% plot per session/animal projections
 tuning_df = pd.read_feather(os.path.join(OUT_PATH, "tuning_df.feat"))
-for algo, params in {"pca": dict(), "isomap": {"n_neighbors": PARAM_NNB}}.items():
-    fig_path = os.path.join(FIG_PATH, algo)
-    os.makedirs(fig_path, exist_ok=True)
-    for (anm, ss), tdf in tuning_df.groupby(["animal", "session"], observed=True):
-        evt_tuning = tdf.set_index(["unit_id", "event"])["act"].to_xarray()
-        proj_df = reduce_wrap(
-            algo,
-            evt_tuning,
-            PARAM_NCOMP,
-            samp_dim="unit_id",
-            feat_dim="event",
-            whiten=PARAM_WHITEN,
-            **params
-        )
-        proj_df = proj_df.reset_index()
-        proj_df["region"] = proj_df["unit_id"].map(
-            tdf.set_index("unit_id")["region"].to_dict()
-        )
-        if PARAM_TRIM_UNKNOWN:
-            proj_df = proj_df.dropna()
-        else:
-            proj_df["region"] = proj_df["region"].fillna("unknown")
-        fig = px.scatter_3d(
-            proj_df,
-            x="comp0",
-            y="comp1",
-            z="comp2",
-            color="region",
-            color_discrete_map={"unknown": "grey"},
-        )
-        fig.update_traces(marker_size=5)
-        fig.update_layout(legend={"itemsizing": "constant"})
-        fig.write_html(os.path.join(fig_path, "{}-{}.html".format(anm, ss)))
+for trim in [True, False]:
+    for algo, params in {"pca": dict(), "isomap": {"n_neighbors": PARAM_NNB}}.items():
+        fig_path = os.path.join(FIG_PATH, algo)
+        os.makedirs(fig_path, exist_ok=True)
+        for (anm, ss), tdf in tuning_df.groupby(["animal", "session"], observed=True):
+            evt_tuning = tdf.set_index(["unit_id", "event"])["act"].to_xarray()
+            proj_df = reduce_wrap(
+                algo,
+                evt_tuning,
+                PARAM_NCOMP,
+                samp_dim="unit_id",
+                feat_dim="event",
+                whiten=PARAM_WHITEN,
+                **params
+            )
+            proj_df = proj_df.reset_index()
+            proj_df["region"] = proj_df["unit_id"].map(
+                tdf.set_index("unit_id")["region"].to_dict()
+            )
+            if trim:
+                proj_df = proj_df.dropna()
+            else:
+                proj_df["region"] = proj_df["region"].fillna("unknown")
+            fig = px.scatter_3d(
+                proj_df,
+                x="comp0",
+                y="comp1",
+                z="comp2",
+                color="region",
+                color_discrete_map={"unknown": "grey"},
+            )
+            fig.update_traces(marker_size=5)
+            fig.update_layout(legend={"itemsizing": "constant"})
+            fig.write_html(
+                os.path.join(
+                    fig_path, "{}-{}-{}.html".format(anm, ss, "trim" if trim else "all")
+                )
+            )
 
 # %% plot across animals
 tuning_df = pd.read_feather(os.path.join(OUT_PATH, "tuning_df.feat"))
-for algo, params in {"pca": dict(), "isomap": {"n_neighbors": PARAM_NNB}}.items():
-    fig_path = os.path.join(FIG_PATH, algo)
-    os.makedirs(fig_path, exist_ok=True)
-    for ss, tdf in tuning_df.groupby("session"):
-        tdf["uid"] = tdf["animal"].astype(str) + "-" + tdf["unit_id"].astype(str)
-        evt_tuning = tdf.set_index(["uid", "event"])["act"].to_xarray().fillna(0)
-        proj_df = reduce_wrap(
-            algo,
-            evt_tuning,
-            PARAM_NCOMP,
-            samp_dim="uid",
-            feat_dim="event",
-            whiten=PARAM_WHITEN,
-            **params
-        )
-        proj_df = proj_df.reset_index()
-        proj_df["region"] = proj_df["uid"].map(tdf.set_index("uid")["region"].to_dict())
-        if PARAM_TRIM_UNKNOWN:
-            proj_df = proj_df.dropna()
-        else:
-            proj_df["region"] = proj_df["region"].fillna("unknown")
-        fig = px.scatter_3d(
-            proj_df,
-            x="comp0",
-            y="comp1",
-            z="comp2",
-            color="region",
-            color_discrete_map={"unknown": "grey"},
-        )
-        fig.update_traces(marker_size=5)
-        fig.update_layout(legend={"itemsizing": "constant"})
-        fig.write_html(os.path.join(fig_path, "all-{}.html".format(ss)))
+for trim in [True, False]:
+    for algo, params in {"pca": dict(), "isomap": {"n_neighbors": PARAM_NNB}}.items():
+        fig_path = os.path.join(FIG_PATH, algo)
+        os.makedirs(fig_path, exist_ok=True)
+        for ss, tdf in tuning_df.groupby("session"):
+            tdf["uid"] = tdf["animal"].astype(str) + "-" + tdf["unit_id"].astype(str)
+            evt_tuning = tdf.set_index(["uid", "event"])["act"].to_xarray().fillna(0)
+            proj_df = reduce_wrap(
+                algo,
+                evt_tuning,
+                PARAM_NCOMP,
+                samp_dim="uid",
+                feat_dim="event",
+                whiten=PARAM_WHITEN,
+                **params
+            )
+            proj_df = proj_df.reset_index()
+            proj_df["region"] = proj_df["uid"].map(
+                tdf.set_index("uid")["region"].to_dict()
+            )
+            if trim:
+                proj_df = proj_df.dropna()
+            else:
+                proj_df["region"] = proj_df["region"].fillna("unknown")
+            fig = px.scatter_3d(
+                proj_df,
+                x="comp0",
+                y="comp1",
+                z="comp2",
+                color="region",
+                color_discrete_map={"unknown": "grey"},
+            )
+            fig.update_traces(marker_size=5)
+            fig.update_layout(legend={"itemsizing": "constant"})
+            fig.write_html(
+                os.path.join(
+                    fig_path, "all-{}-{}.html".format(ss, "trim" if trim else "all")
+                )
+            )
